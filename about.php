3a6f162aca4591b55db56624881adcc8549fc188
Ram Itkar <141602437+ramitkar0102@users.noreply.github.com>
2023-08-07 09:18:54 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,103 @@
+<?php include 'header.php';?>
+
+
+<!-- Content -->
+<div id="content">
+
+    <!-- Intro -->
+    <section class="p-t-b-150">
+        <div class="container">
+            <div class="intro-main">
+                <div class="row">
+
+                    <!-- Intro Detail -->
+                    <div class="col-md-7">
+                        <div class="text-sec padding-right-0">
+                            <h5>Health Check Ups</h5>
+                            <ul class="row">
+                                 <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                                    <style>
+                                    
+                                    table, th, td {
+                                        border: 1px solid black;
+  background-color: pink;
+  padding: 12px;
+  
+  
+  
+}
+
+</style>
+<body>
+
+<table style="width:100%">
+  <tr>
+    <td>Day</td>
+    <td>Time</td>
+    <td>Doctor Name</td>
+    <td>Department</td>
+  </tr>
+   <tr>
+    <td>Monday</td>
+    <td><span>8.00 AM - 10.00 PM</span></td>
+    <td>Ram Itkar</td>
+    <td>Cardiology</td>
+  </tr>
+  <tr>
+    <td>Tuesday</td>
+    <td><span>8.00 AM - 10.00 PM</span></td>
+    <td>Shailesh Bhange</td>
+    <td>Orthopedics</td>
+  </tr>
+  <tr>
+    <td>Wednesday</td>
+    <td><span>8.00 AM - 2.00 PM</span></td>
+    <td>Mudit Srivastava</td>
+    <td>Neurology</td>
+  </tr>
+  <tr>
+    <td>Thursday</td>
+    <td><span>8.00 AM - 10.00 PM</span></td>
+    <td>Jatin Shevkari</td>
+    <td>Dermatology</td>
+  </tr>
+  <tr>
+    <td>Friday</td>
+    <td><span>8.00 AM - 10.00 PM</span></td>
+    <td>Rahul Itkar</td>
+    <td>General Surgery</td>
+  </tr>
+  <tr>
+    <td>Saturday</td>
+    <td><span>8.00 AM - 10.00 PM</span></td>
+    <td>Ravi Itkar</td>
+    <td>General Medicine</td>
+  </tr>
+
+
+</table>
+
+</body>
+                    
+
+                                    </li>
+                                <</ul>
+                        </div>
+                    </div>
+
+                    <!-- Intro Timing -->
+                    <div class="col-md-5"> <img class="img-responsive intro-img" src="images/intro-img.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+<!-- Footer -->
+<!--======= FOOTER =========-->
+
+<?php include 'footer.php';?>
---
+++ index.php
@@ -0,0 +1,276 @@
+
+  <?php include 'header.php';?>
+  <!-- Bnr Header -->
+  <section class="main-banner">
+    <div class="tp-banner-container">
+      <div class="tp-banner">
+        <ul>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms.jpg"  alt="slider"  data-bgposition=" center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <!-- LAYER NR. 1 -->
+            <div class="tp-caption sfl tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-120" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:50px; font-weight:500; color:black;  max-width: auto; max-height: auto; white-space: nowrap;">Online Doctor Appointment System</div>
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfr tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-60" 
+                data-speed="800" 
+                data-start="1000" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 6; font-size:40px; color:gray; font-weight:500; white-space: nowrap;">We care about your health </div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="0" 
+                data-speed="800" 
+                data-start="1200" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="none" 
+                data-splitout="none" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 7;  font-size:22px; color:gray; font-weight:500; max-width: auto; max-height: auto; white-space: nowrap;">Best Hospitality Services in your town</div>
+            
+            <!-- LAYER NR. 4 -->
+            <div class="tp-caption lfb tp-resizeme scroll" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="120"
+                data-speed="800" 
+                data-start="1300"
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 8;"><a href="patientappointment.php" class="btn">Book Now</a> </div>
+          </li>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms2.jpg"  alt="slider"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <!-- LAYER NR. 1 -->
+            <div class="tp-caption sfl tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="-100" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:40px; font-weight:500; color:#000;  max-width: auto; max-height: auto; white-space: nowrap;">Best Diagnostic Centre </div>
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfr tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="-40" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:55px; font-weight:500; color:#000;  max-width: auto; max-height: auto; white-space: nowrap;">Care And Cure</div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="30" 
+                data-speed="800" 
+                data-start="1400" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="none" 
+                data-splitout="none" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 7; font-size:16px; color:#000; font-weight:500; line-height:26px; max-width: auto; max-height: auto; white-space: nowrap;">Improved diagnostic performance and heightened satisfaction of patients <br> and physicians delight.</div>
+            
+            <!-- LAYER NR. 4 -->
+            <div class="tp-caption lfb tp-resizeme scroll" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="140"
+                data-speed="800" 
+                data-start="1600"
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 8;"><a href="contact.php" class="btn">CONTACT NOW</a> </div>
+          </li>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms3.jpg"  alt="slider"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-80" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 6; font-size:40px; color:#000; font-weight:500; white-space: nowrap;"> Welcome To Our Research Center </div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme text-center" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-10" 
+                data-speed="800" 
+                data-start="1000" 
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 7; font-size:20px; font-weight:500; line-height:26px; color:#000; max-width: auto; max-height: auto; white-space: nowrap;"><br>
+              technologies and sharing our expertise.</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  
+  <!-- Content -->
+  <div id="content"> 
+    
+    <!-- Intro -->
+    <section class="p-t-b-150">
+      <div class="container">
+        <div class="intro-main">
+          <div class="row"> 
+            
+            <!-- Intro Detail -->
+            <div class="col-md-8">
+              <div class="text-sec">
+                <h5><b>Health Check Ups</b></h5>
+                <ul class="row">
+                    <style>
+table, th, td {
+  border:1px solid black;
+  background-color: aqua;
+  
+  
+}
+</style>
+<body>
+
+<table style="width:100%">
+  <tr>
+    <td><h6><b>Day</b></h6></td>
+    <td><h6><b>Time</b></h6></td>
+    <td><h6><b>Doctor Name</b></h6></td>
+    <td><h6><b>Department</b></h6></td>
+  </tr>
+   <tr>
+    <td>Monday</td>
+    <td><span>9.00 - 11.00</span></td>
+    <td>Prajval Gandhi</td>
+    <td>Cardiology</td>
+  </tr>
+  <tr>
+    <td>Tuesday</td>
+    <td><span>11.00 - 1.00</span></td>
+    <td>Amaan Khan</td>
+    <td>Orthopedics</td>
+  </tr>
+  <tr>
+    <td>Wednesday</td>
+    <td><span>12.00 - 2.00</span></td>
+    <td>Vaibhav Dawane</td>
+    <td>Neurology</td>
+  </tr>
+  <tr>
+    <td>Thursday</td>
+    <td><span>2.00 - 4.00</span></td>
+    <td>Shreyash Holkar</td>
+    <td>Dermatology</td>
+  </tr>
+  <tr>
+    <td>Friday</td>
+    <td><span>3.00 - 5.00</span></td>
+    <td>Rushi Chaudhari</td>
+    <td>General Surgery</td>
+  </tr>
+  <tr>
+    <td>Saturday</td>
+    <td><span>10.00 - 12.00</span></td>
+    <td>Prajwal Itkar</td>
+    <td>General Medicine</td>
+  </tr>
+
+
+</table>
+
+</body>
+                    
+                  
+              </div>
+            </div>
+            
+            <!-- Intro Timing -->
+            <div class="col-md-4">
+              <div class="timing"> <i class="lnr lnr-clock"></i>
+                <ul>
+                  <li> Monday <span>8.00 AM - 10.00 PM</span></li>
+                  <li> Tuesday <span>8.00 AM - 10.00 PM</span></li>
+                  <li> Wednesday <span>8.00 AM - 10.00 PM</span></li>
+                  <li> Thursday <span>8.00 AM - 10.00 PM</span></li>
+                  <li> Friday <span>8.00 AM - 10.00 PM</span></li>
+                  <li> Saturday <span>8.00 AM - 04.00 PM</span></li>
+                  <li> Sunday <span>10.00 AM - 02.00 PM</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  
+
+    
+    <!-- DOCTOR LIST -->
+    <section class="p-t-b-150">
+      <div class="container"> 
+      
+      
+        
+        
+  
+  <!-- Footer -->
+<?php include 'footer.php';?>
